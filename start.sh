56cf4b015c089600c12b1a7f360cca320317d5e6
Alexander Martinz <eviscerationls@gmail.com>
2016-04-21 10:27:32 +0200

    add start script
    Change-Id: Ib4f46f47a509602f398399e083fe3fea7354f616
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# REQUIREMENTS
+#
+# you need to set some environment values for this to work
+#
+# HUBOT_SLACK_TOKEN_NAMELESS -> your hubot token (https://nameless-org.slack.com/services/B125S4V7F)
+# HUBOT_SLACK_WORKING_DIR -> where you have the slack bot installed
+#
+# You need to have "nohup" installed
+#
+
+cd ${HUBOT_SLACK_WORKING_DIR}
+HUBOT_SLACK_TOKEN=${HUBOT_SLACK_TOKEN_NAMELESS} nohup ./bin/hubot -a slack
