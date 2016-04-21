a963814970e4e75abd4ea3cfdb1f8c7b9f8e78b4
Alexander Martinz <eviscerationls@gmail.com>
2016-04-21 10:37:50 +0200

    tools: systemd: use HUBOT_SLACK_WORKING_DIR
    Change-Id: I301cad86b16bb665aa591afceccf725273ee73a4
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- tools/systemd/install.sh
+++ tools/systemd/install.sh
@@ -3,5 +3,5 @@
 # Please edit the service to your needs!
 # It uses a hardcoded path to the start script!
 
-sudo cp /opt/slack/nameless/tools/systemd/slack-bot-nameless.service /etc/systemd/system/
+sudo cp ${HUBOT_SLACK_WORKING_DIR}/tools/systemd/slack-bot-nameless.service /etc/systemd/system/
 sudo systemctl daemon-reload
